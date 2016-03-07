5a41a3d1ade7dd02b0b9534b157749e9a2bead83
James Kim <jameskim1093@gmail.com>
2016-03-06 23:07:07 -0800

    update

--- tidyhome.js
+++ tidyhome.js
@@ -1,29 +1,10 @@
-$(document).ready(function() {
-  console.log("ready");
-    
-$('section#sign-up-screen').hide();
-                  
-$('#sign-up').click(function(){
-    $('#home-screen').fadeOut(); 
-  });
-
-$('#sign-up').click(function(){
-    $('#sign-up-screen').fadeIn();
-});
-
-$('.log-btn').click(function(){
-    $('#home-screen').fadeOut(); 
-  });
-
-$('.log-btn').click(function(){
-    $('#main-screen').fadeIn(); 
-  });
-
-$('#submit-btn').click(function(){
-    $('#sign-up-screen').fadeOut();
-});
-    
-$('#submit-btn').click(function(){
-    $('#main-screen').fadeIn(); 
-  });
-});
+$.backstretch([
+        "images/house.jpg",
+          "images/water.jpg",
+          "images/lights.JPG",
+          "images/subway.jpg",
+          "images/park.JPG"
+        ], {
+            fade: 750,
+            duration: 4000
+        });
